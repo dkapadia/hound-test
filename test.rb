bc0c8e0827fd4e63b0198d519fa761639ffca2eb
Dhruv Kapadia <git@dkapadia.com>
2015-10-19 15:00:23 -0400

    Introduce a line that hound should be commenting on

--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
 class HoundTest
   LINELENGTH = "This line is less than 80 chars"
+  # expect hound to raise an error, but with line length max set to 100
+  LINELENGTH = "Testing that line length is set to 100 as specified in custom config rather than 80"
 end
